182e8a611d6d4e942a1cebeaab6174b1e9befe79
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-05-28 20:07:03 +0530

    Update Lab1A.c

--- Lab1A.c
+++ Lab1A.c
@@ -3,7 +3,7 @@ void linear_search(int val,int n,int arr[*]);		//non-recursive function
 int linear_search_re(int val,int n,int arr[*]);		//recursive function
 void main()
 {
-	int n,i,val,pos;
+	int n,i,val,pos,option;
 	printf("enter the size of array: ");
 	scanf("%d",&n);
 	int arr[n];
@@ -12,12 +12,28 @@ void main()
 		scanf("%d",&arr[i]);
 	printf("enter the value to search: ");
 	scanf("%d",&val);
-	linear_search(val,n,arr);
-	pos=linear_search_re(val,n,arr);
-	if(pos!=0)
-		printf("\nelement %d is found at position %d using recursive function",val,pos);
-	else 
-		printf("\nelement %d does not found using recursive function",val);
+	printf("enter 1 for Non-Recursive Function \n");
+	printf("enter 2 for Recursive Function\n ");
+	scanf("%d",&option);
+	switch(option)
+	{
+	    case 1:
+	    {
+	       	linear_search(val,n,arr);
+	       	break;
+	    }
+	    case 2:
+	    {
+	        pos=linear_search_re(val,n,arr);
+	        if(pos!=0)
+		        printf("\nelement %d is found at position %d using Recursive function",val,pos);
+	        else 
+	        	printf("\nelement %d does not found using Recursive function",val);
+	        break;	
+	    }
+	    default:
+	        printf("enter correct option! ");
+	}
 }
 void linear_search(int val,int n,int arr[n])	//Non-recursive function
 {
@@ -28,12 +44,12 @@ void linear_search(int val,int n,int arr[n])	//Non-recursive function
 		{
 			found=1;
 			pos=i;
-			printf("\nelement %d is found at position %d using non-recursive function",val,i+1);
+			printf("\nelement %d is found at position %d using Non-Recursive function",val,i+1);
 			break;
 		}
 	}
 	if(found==0)
-		printf("\nelement %d does not found using non-recursive function",val);
+		printf("\nelement %d does not found using Non-Recursive function",val);
 }
 int linear_search_re(int val,int n,int arr[n])	  //Recursive function
 {
@@ -44,4 +60,5 @@ int linear_search_re(int val,int n,int arr[n])	  //Recursive function
 		else
 			return linear_search_re(val,n-1,arr);	
 	}
+	return 0;
 }
